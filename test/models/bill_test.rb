876e49c620bb4b0cc39960b3601fdf09ef90ea31
Topstitch <kallen@spoonflower.com>
2015-02-20 14:42:08 -0500

    Clarified some more wording

--- test/models/bill_test.rb
+++ test/models/bill_test.rb
@@ -7,10 +7,14 @@ class BillTest < ActiveSupport::TestCase
   test "timeframe" do
     assert_equal "Upcoming", bills(:two).timeframe
     assert_equal "Past", bills(:one).timeframe
+    assert_equal "Upcoming", bills(:three).timeframe
+    assert_equal "Past", bills(:four).timeframe
   end
 
   test "yes_no" do
     assert_equal "Yes", bills(:two).yes_no
     assert_equal "No", bills(:one).yes_no
+    assert_equal "No", bills(:three).yes_no
+    assert_equal "Yes", bills(:four).yes_no
   end
 end
