5d6c3821926bd4ee724a67148c156fa670e5db1d
Ruben <ssrcruz@gmail.com>
2015-02-20 14:24:52 -0500

    Create timeframe, yes_no tests

--- test/models/bill_test.rb
+++ test/models/bill_test.rb
@@ -4,6 +4,13 @@ class BillTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "timeframe" do
+    assert_equal "Upcoming", bills(:two).timeframe
+    assert_equal "Past", bills(:one).timeframe
+  end
 
-
+  test "yes_no" do
+    assert_equal "Yes", bills(:two).yes_no
+    assert_equal "No", bills(:one).yes_no
+  end
 end
